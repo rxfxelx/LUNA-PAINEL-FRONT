db8f68ff376861f0bad81c4ccebb5b869fb9e469
rafael sena <89745814+rxfxelx@users.noreply.github.com>
2025-09-15 15:00:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
 // config.js
 window.__BACKEND_URL__ = "https://web-production-3bc4c.up.railway.app"
+
+// === Configurações da GetNet (homologação) ===
+window.__GETNET_SELLER_ID__ = "17943e93-e7d2-423a-a026-ac3368bad649"
+window.__GETNET_CLIENT_ID__ = "8c4dbf87-fece-4349-8771-8fe6e261b241"
+window.__GETNET_CLIENT_SECRET__ = "f435dd6e-6076-4dd2-a92d-9a13f0d8f527"
+window.__GETNET_ENV__ = "sandbox" // 'sandbox' | 'production'
