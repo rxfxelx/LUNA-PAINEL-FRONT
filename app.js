77d6526a8b4ddffb66cf7a0c29d71b789f9ebd3a
rafael sena <89745814+rxfxelx@users.noreply.github.com>
2025-09-15 13:45:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -314,7 +314,7 @@ async function createCheckoutLink() {
           <rect x="1" y="4" width="22" height="16" rx="2" ry="2"/>
           <line x1="1" y1="10" x2="23" y2="10"/>
         </svg>
-        Pagar com GetNet
+        Assinar agora
       `
     }
   }
